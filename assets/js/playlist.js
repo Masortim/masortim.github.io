e459590cdb71c8823d8ba567ca715d62e6637065
Shmulik <52025854+Masortim@users.noreply.github.com>
2021-12-24 18:26:28 +0700

    Update playlist.js

--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -114,7 +114,7 @@ var myPlayListPlayer;
           containment: 'body',
           autoPlay: true,
           mute: true,
-          startAt: 22,
+          startAt: 23,
           // stopAt: 363,
           opacity: 1,
           loop: false,
