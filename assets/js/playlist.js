8c779233fa71c6d36143970d8ceebc58598f6dee
Shmulik <52025854+Masortim@users.noreply.github.com>
2021-12-24 17:53:55 +0700

    Create playlist.js

---
+++ assets/js/playlist.js
@@ -0,0 +1,119 @@
+var myPlayListPlayer;
+    jQuery(function () {
+      /**
+       * Set the video list with all the parameters for each video
+       * @type {*[]}
+       */
+      var videos = [
+        {
+          videoURL: "k_okcNVZqqI", // INK DROPS
+          containment: 'body',
+          autoPlay: true,
+          mute: true,
+          startAt: 0,
+          stopAt: 159,
+          opacity: 1,
+          loop: false,
+          ratio: 4/3,
+          addRaster: true,
+          quality: "large",
+        },
+        {
+          videoURL: "AaA246e4u_A", // Drippy Eye Projections Oil Wheel 056
+          containment: 'body',
+          autoPlay: true,
+          mute: true,
+          startAt: 0,
+          // stopAt: 75,
+          opacity: 1,
+          loop: false,
+          ratio: 4/3,
+          addRaster: true,
+          quality: "large",
+        },
+        {
+          videoURL: "XVkADAwOXnU", // лучший расслабляющий аквариум
+          containment: 'body',
+          autoPlay: true,
+          mute: true,
+          startAt: 0,
+          // stopAt: 75,
+          opacity: 1,
+          loop: false,
+          ratio: 4/3,
+          addRaster: true,
+          quality: "large",
+        },
+        {
+          videoURL: "lyh2kAjcmSY", // Russia 4K
+          containment: 'body',
+          autoPlay: true,
+          mute: true,
+          startAt: 11,
+          // stopAt: 75,
+          opacity: 1,
+          loop: false,
+          ratio: 4/3,
+          addRaster: true,
+          quality: "large",
+        },
+        {
+          videoURL: "cg_kcaymWK0", // 京都の紅葉60選 : The 60 Best Autumn Leaves Spots In Kyoto
+          containment: 'body',
+          autoPlay: true,
+          mute: true,
+          startAt: 5,
+          // stopAt: 75,
+          opacity: 1,
+          loop: false,
+          ratio: 4/3,
+          addRaster: true,
+          quality: "large",
+        },
+        {
+          videoURL: "eRUQPfVACs0", // Norway in 8K ULTRA HD HDR - Most peaceful Country in the World
+          containment: 'body',
+          autoPlay: true,
+          mute: true,
+          startAt: 8,
+          stopAt: 1160,
+          opacity: 1,
+          loop: false,
+          ratio: 4/3,
+          addRaster: true,
+          quality: "large",
+        },
+        {
+          videoURL: "JABGEOY8XS4", // Рождение тритона
+          containment: 'body',
+          autoPlay: true,
+          mute: true,
+          startAt: 10,
+          stopAt: 363,
+          opacity: 1,
+          loop: false,
+          ratio: 4/3,
+          addRaster: true,
+          quality: "large",
+        }
+      ];
+    let filters = {
+      brightness: 50,
+    }
+    jQuery('#myPlayerID').YTPApplyFilters(filters);
+
+      myPlayListPlayer = jQuery("#myPlayerID").YTPlaylist(videos, true, function (video) {
+        /*
+                        if (video.videoData) {
+                            jQuery("#videoID").html(video.videoData.id);
+                            jQuery("#videoTitle").html(video.videoData.title);
+                        }
+        */
+      });
+
+      myPlayListPlayer.on("YTPData", function (e) {
+        jQuery("#videoID").html(e.prop.id);
+        jQuery("#videoTitle").html(e.prop.title);
+      });
+
+    });
