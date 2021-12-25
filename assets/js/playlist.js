a81109807c497bcd61a3fcf803d636ed46c12964
Shmulik <52025854+Masortim@users.noreply.github.com>
2021-12-25 19:48:53 +0700

    Update playlist.js

--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -290,6 +290,19 @@ var myPlayListPlayer;
           ratio: 4/3,
           addRaster: true,
           quality: "large",
+        },
+        {
+          videoURL: "nYwA6phqNEA", // Imminence - Ghost [Visual Experience]
+          containment: 'body',
+          autoPlay: true,
+          mute: true,
+          startAt: 0,
+          // stopAt: 120,
+          opacity: 1,
+          loop: false,
+          ratio: 4/3,
+          addRaster: true,
+          quality: "large",
         }
       ];
     let filters = {
