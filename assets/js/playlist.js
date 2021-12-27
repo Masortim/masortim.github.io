6896df9dfd9af1e94a5afe72e1cd750551526622
Shmulik <52025854+Masortim@users.noreply.github.com>
2021-12-27 15:42:11 +0700

    Update playlist.js

--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -277,6 +277,19 @@ var myPlayListPlayer;
           ratio: 4/3,
           addRaster: true,
           quality: "large",
+        },
+        {
+          videoURL: "GJYrMhJxTrs", // Россия Drone Film (4K UHD) с успокаивающей музыкой
+          containment: 'body',
+          autoPlay: true,
+          mute: true,
+          startAt: 0,
+          // stopAt: 120,
+          opacity: 1,
+          loop: false,
+          ratio: 4/3,
+          addRaster: true,
+          quality: "large",
         }
       ];
     let filters = {
